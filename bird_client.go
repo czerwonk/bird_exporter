0619a4c05c3b51cf8fd00fe237a602bb5ddaec9e
Daniel Czerwonk <daniel@dan-nrw.de>
2017-06-30 16:57:40 +0200

    using czerwonk/bird_socket lib for communication with bird

--- bird_client.go
+++ bird_client.go
@@ -1,24 +1,9 @@
 package main
 
 import (
-	"net"
-
-	"regexp"
-
-	"time"
-
 	"github.com/czerwonk/bird_exporter/protocol"
-	"github.com/prometheus/common/log"
+	"github.com/czerwonk/bird_socket"
 )
-
-const bufferSize = 4096
-const timeout = 30
-
-var birdReturnCodeRegex *regexp.Regexp
-
-func init() {
-	birdReturnCodeRegex = regexp.MustCompile("\\d{4} \n$")
-}
 
 func getProtocols() ([]*protocol.Protocol, error) {
 	protocols := make([]*protocol.Protocol, 0)
@@ -43,56 +28,10 @@ func getProtocols() ([]*protocol.Protocol, error) {
 }
 
 func getProtocolsFromBird(socketPath string, ipVersion int) ([]*protocol.Protocol, error) {
-	conn, err := net.Dial("unix", socketPath)
-	if err != nil {
-		return nil, err
-	}
-	defer conn.Close()
-
-	conn.SetReadDeadline(time.Now().Add(timeout * time.Second))
-
-	buf := make([]byte, bufferSize)
-	n, err := conn.Read(buf[:])
-	if err != nil {
-		return nil, err
-	}
-	log.Debug(string(buf[:n]))
-
-	_, err = conn.Write([]byte("show protocols all\n"))
+	b, err := bird_socket.Query(socketPath, "show protocols all")
 	if err != nil {
 		return nil, err
 	}
 
-	output, err := readFromSocket(conn)
-	if err != nil {
-		return nil, err
-	}
-
-	return parseOutput(output, ipVersion), nil
-}
-
-func readFromSocket(conn net.Conn) ([]byte, error) {
-	b := make([]byte, 0)
-	buf := make([]byte, bufferSize)
-
-	done := false
-	for !done {
-		n, err := conn.Read(buf[:])
-		if err != nil {
-			return nil, err
-		}
-
-		b = append(b, buf[:n]...)
-		done = endsWithBirdReturnCode(b)
-	}
-
-	return b, nil
-}
-
-func endsWithBirdReturnCode(b []byte) bool {
-	if len(b) < 6 {
-		return false
-	}
-
-	return birdReturnCodeRegex.Match(b[len(b)-6:])
+	return parseOutput(b, ipVersion), nil
 }
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/prometheus/common/log"
 )
 
-const version string = "0.8.1"
+const version string = "0.8.2"
 
 var (
 	showVersion   = flag.Bool("version", false, "Print version information.")
