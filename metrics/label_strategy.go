42bfdbb0fcfa4e80264f0000c85d78c3810012bb
Daniel Czerwonk <czerwonk@users.noreply.github.com>
2018-08-09 11:34:24 +0200

    Update label_strategy.go

--- metrics/label_strategy.go
+++ metrics/label_strategy.go
@@ -2,7 +2,11 @@ package metrics
 
 import "github.com/czerwonk/bird_exporter/protocol"
 
+// LabelStrategy abstracts the label generation for protocol metrics
 type LabelStrategy interface {
+	// LabelNames is the list of label names
 	LabelNames() []string
+	
+	// Label values is the list of values for the labels specified in `LabelNames()` 
 	LabelValues(p *protocol.Protocol) []string
 }
