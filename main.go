9724596dbf2ee84d6899bc3b8043676c942824c0
Daniel Czerwonk <daniel@dan-nrw.de>
2022-01-27 10:34:17 +0100

    bump version

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-const version string = "1.3.0"
+const version string = "1.4.0"
 
 var (
 	showVersion   = flag.Bool("version", false, "Print version information.")
