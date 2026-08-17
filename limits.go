5ea3c01cf55d5c56ad5fcc99d9e637af76308bce
Christoph Petrausch <263448+hikhvar@users.noreply.github.com>
2026-08-17 21:00:47 +0200

    feat: Implement support for CPU and memory limits automatically detected from the container environment. (#149)
    This helps to get a lower resource consumption on very big hardware nodes

---
+++ limits.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/KimMachineGun/automemlimit/memlimit"
+	log "github.com/sirupsen/logrus"
+	"go.uber.org/automaxprocs/maxprocs"
+)
+
+func init() {
+	_, err := maxprocs.Set(maxprocs.Logger(log.Debugf))
+	if err != nil {
+		log.Error("failed to setup CPU limits, continue without limits: %v", err)
+	}
+	_, err = memlimit.SetGoMemLimitWithOpts(
+		memlimit.WithRatio(0.9),
+		memlimit.WithProvider(
+			memlimit.ApplyFallback(
+				memlimit.FromCgroup,
+				memlimit.FromSystem,
+			),
+		),
+	)
+	if err != nil {
+		log.Error("failed to setup memory limits, continue without limits: %v", err)
+	}
+}
